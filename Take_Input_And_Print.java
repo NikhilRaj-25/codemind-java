b378c3aa1676db20dddc186518c9e64e1b111b2a
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-08-30 18:02:18 +0530

    import java.util.*; public class file{     public static void main(String[] args){         Scanner ab=new Scanner(System.in);         String a=ab.nextLine();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        Scanner ab=new Scanner(System.in);
+        String a=ab.nextLine();
+        System.out.println(a);
+    }
+}
