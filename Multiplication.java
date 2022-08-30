c6c90a461af8d1e2b6f7d740819a0dbeb17d90f5
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-08-30 17:51:07 +0530

    import java.util.*; public class file{     public static void main(String[] args){         Scanner ab=new Scanner(System.in);         int a=ab.nextInt();         int b=ab.nextInt();         System.out.println(a*b);     } }

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        Scanner ab=new Scanner(System.in);
+        int a=ab.nextInt();
+        int b=ab.nextInt();
+        System.out.println(a*b);
+    }
+}
