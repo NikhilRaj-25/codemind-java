d3ad67fd41f828cc59f8e6d31a6b68103735a61d
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-09-01 11:16:31 +0530

    import java.util.*; public class file{     public static void main(String[] args){         Scanner ab=new Scanner(System.in);         double c,f;         c=ab.nextDouble();         f=32+(c*1.8);         System.out.printf("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        Scanner ab=new Scanner(System.in);
+        double c,f;
+        c=ab.nextDouble();
+        f=32+(c*1.8);
+        System.out.printf("%.2f",f);
+    }
+}
