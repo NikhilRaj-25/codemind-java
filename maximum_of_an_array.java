ddf6534f2b2c2e6e84e94f3cb502f5c27430f4ca
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-09-09 15:27:00 +0530

    import java.util.*; public class file{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int x[]=new int[a];         int b=-9999;         for(int i=0;i<a;i++){             x[i]=sc.nextInt();         }         for(int i=0;i<a;i++){             if(b<x[i]){                 b=x[i];             }         }         System.out.println(b);     } }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int x[]=new int[a];
+        int b=-9999;
+        for(int i=0;i<a;i++){
+            x[i]=sc.nextInt();
+        }
+        for(int i=0;i<a;i++){
+            if(b<x[i]){
+                b=x[i];
+            }
+        }
+        System.out.println(b);
+    }
+}
