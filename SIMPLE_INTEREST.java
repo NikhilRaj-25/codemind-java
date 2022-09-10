e24f6bb81baa25af203f2fafe6eb01f302ee6f3e
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-09-11 00:41:38 +0530

    import java.util.*; public class file{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int p=sc.nextInt();         int t=sc.nextInt();         int r=sc.nextInt();         int si=(p*t*r)/100;         System.out.println(si);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int t=sc.nextInt();
+        int r=sc.nextInt();
+        int si=(p*t*r)/100;
+        System.out.println(si);
+    }
+}
