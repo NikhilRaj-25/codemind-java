3de64ada6693ddf11fb4a0fe697cfc0be9573cb2
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-09-25 22:56:41 +0530

    import java.util.*; public class file{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         for(int i=a;i>=1;i--){             for(int j=i;j>=1;j--){                 System.out.print((char)(64+i)+" ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -3,9 +3,9 @@ public class file{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        for(int i=1;i<=a;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(j);
+        for(int i=a;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print((char)(64+i)+" ");
             }
             System.out.println();
         }
