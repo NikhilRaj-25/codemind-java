2325206aa82a2c32c0ac33f38ade589474e6b44b
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-10-21 23:11:42 +0530

    import java.util.*; public class file{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int a1[]=new int[a];         int a2[]=new int[a];         for(int i=0;i<a;i++){             a1[i]=sc.nextInt();         }         for(int i=0;i<a;i++){             a2[i]=sc.nextInt();         }         for(int i=0;i<a;i++){             a         }     } }

---
+++ Monk_and_Welcome_Problem.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class file{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int a1[]=new int[a];
+        int a2[]=new int[a];
+        for(int i=0;i<a;i++){
+            a1[i]=sc.nextInt();
+        }
+        for(int i=0;i<a;i++){
+            a2[i]=sc.nextInt();
+        }
+        for(int i=0;i<a;i++){
+            a
+        }
+    }
+}
