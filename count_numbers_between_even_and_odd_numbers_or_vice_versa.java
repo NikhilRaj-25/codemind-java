31250ff8c995c1ac49b4abf05ba6f76ff538ac98
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-11-05 22:00:35 +0530

    import java.util.*; public class file{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int arr[]=new int[a];         for(int i=0;i<a;i++){             arr[i]=sc.nextInt();         }         int c=0;         for(int i=0;i<a-2;i++){             if(arr[i]%2!=0 && arr[i+2]%2==0){                 c+=1;             }             if(arr[i]%2==0 && arr[i+2]%2!=0){                 c+=1;             }         }         System.out.println(c);     } }

---
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class file{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        for(int i=0;i<a;i++){
+            arr[i]=sc.nextInt();
+        }
+        int c=0;
+        for(int i=0;i<a-2;i++){
+            if(arr[i]%2!=0 && arr[i+2]%2==0){
+                c+=1;
+            }
+            if(arr[i]%2==0 && arr[i+2]%2!=0){
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
